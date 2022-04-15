06d807746ac40a82e92aa4bdeaaade4a0aee5d2e
claudio.arcidiacono <claudio.arcidiacono@ing.com>
2022-04-15 16:08:12 +0200

    Add key platform-dependend key replacement

---
+++ js/replace_keys.js
@@ -0,0 +1,12 @@
+if ((window.navigator?.userAgentData?.platform ?? window.navigator.platform).includes("mac")) {
+
+    html = document.querySelector(".post-content").innerHTML
+    to_replace = {
+        "<kbd>Alt</kbd>": "<kbd>⌥Opt</kbd>",
+        "<kbd>Ctrl</kbd>": "<kbd>⌘Cmd</kbd>",
+    }
+    for (const [key, value] of Object.entries(to_replace)) {
+        html = html.replace(new RegExp(key, 'g'), value);
+    }
+    document.querySelector(".post-content").innerHTML = html
+}
